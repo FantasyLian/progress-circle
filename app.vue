dcb1236befee7de8924d8ae0bf0ca61d7a955245
Mr.cat <fantasy.lian@163.com>
2020-04-25 23:49:19 +0800

    add components

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <progress-circle></progress-circle>
+    </div>
+</template>
+<script>
+import progressCircle from './components/progress-circle.vue'
+export default {
+    name: 'app',
+    components: { progressCircle }
+}
+</script>
+<style lang="less">
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
